19ee08f68b613b4f8875aa33618e88113085b17f
Cole Johnson <cburnjohnson@gmail.com>
2020-03-20 20:42:14 -0700

    ocean theme

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const bodyClass = document.body.classList;
+
+// Sun Button Event Listener
 document.querySelector('.sun-button').addEventListener('click', () => {
-    if (bodyClass[0] === 'moon-theme') {
-        bodyClass.remove('moon-theme');
+    switch (bodyClass[0]) {
+        case 'moon-theme':
+            bodyClass.remove('moon-theme');
+            break;
+        case 'ocean-theme':
+            bodyClass.remove('ocean-theme');
+            break;
+        default:
+            break;
     }
     document.body.classList.add('sun-theme');
 });
+
+// Moon Button Event Listener
 document.querySelector('.moon-button').addEventListener('click', () => {
-    if (bodyClass[0] === 'sun-theme') {
-        bodyClass.remove('sun-theme');
+    switch (bodyClass[0]) {
+        case 'sun-theme':
+            bodyClass.remove('sun-theme');
+            break;
+        case 'ocean-theme':
+            bodyClass.remove('ocean-theme');
+            break;
+        default:
+            break;
     }
-    document.body.classList.toggle('moon-theme');
+    document.body.classList.add('moon-theme');
+});
+
+// Ocean Button Event Listener
+document.querySelector('.ocean-button').addEventListener('click', () => {
+    switch (bodyClass[0]) {
+        case 'sun-theme':
+            bodyClass.remove('sun-theme');
+            break;
+        case 'moon-theme':
+            bodyClass.remove('moon-theme');
+            break;
+        default:
+            break;
+    }
+    document.body.classList.add('ocean-theme');
 });
